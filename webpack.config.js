5384c58e6c84ab3c9039540922e322fc0bc587b0
pasinduMendis <pasindu.mendis97@gmail.com>
2023-12-26 12:43:31 +0530

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
-module.exports= {
-    rules: [
-      {
-        test: /\.js|\.jsx$/,
-        exclude: /node_modules/,
-      }
-    ]
+const path = require("path");
+
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module:{
+        rules:[{
+            loader: 'babel-loader',
+            test: /\.js$|jsx/,
+            exclude: /node_modules/
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
 }
